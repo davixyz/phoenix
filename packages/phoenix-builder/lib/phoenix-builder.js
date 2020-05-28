08848b3007c4311d2854b4372962c00a9637c4fd
Carlos Castro <cdcastrofelix@gmail.com>
2020-05-27 21:44:26 -0700

    feat: add rollup support

--- packages/phoenix-builder/lib/phoenix-builder.js
+++ packages/phoenix-builder/lib/phoenix-builder.js
@@ -1,3 +1,49 @@
 #!/usr/bin/env node
+const rollup = require('rollup');
+const path = require('path');
+const resolve = require('@rollup/plugin-node-resolve').default;
+const babel = require('@rollup/plugin-babel').default;
 
-console.log('Woo');
+const currentWorkingPath = process.cwd();
+const { main, name } = require(path.join(currentWorkingPath, 'package.json'));
+
+const inputPath = path.join(currentWorkingPath, main);
+const outputPath = path.join(currentWorkingPath, 'dist');
+
+const fileName = name.replace('@carlosdev/', '');
+console.log(fileName);
+
+// see below for details on the options
+const inputOptions = {
+  input: inputPath,
+  external: ['react'],
+  plugins: [
+    resolve(),
+    babel({
+      presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+      babelHelpers: 'bundled',
+    }),
+  ],
+};
+const outputOptions = [
+  {
+    file: `dist/${fileName}.cjs.js`,
+    format: 'cjs',
+  },
+  {
+    file: `dist/${fileName}.esm.js`,
+    format: 'esm',
+  },
+];
+
+async function build() {
+  // create a bundle
+  const bundle = await rollup.rollup(inputOptions);
+  // or write the bundle to disk
+  // await bundle.write(outputOptions);
+  outputOptions.forEach(async (options) => {
+    await bundle.write(options);
+  });
+}
+
+build();
