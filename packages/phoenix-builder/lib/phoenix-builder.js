7c79277eae6ef3455882a4154a8c541597d98ed4
Carlos Castro <cdcastrofelix@gmail.com>
2020-05-28 21:47:11 -0700

    feat: add button css and wire distribuition

--- packages/phoenix-builder/lib/phoenix-builder.js
+++ packages/phoenix-builder/lib/phoenix-builder.js
@@ -3,6 +3,7 @@ const rollup = require('rollup');
 const path = require('path');
 const resolve = require('@rollup/plugin-node-resolve').default;
 const babel = require('@rollup/plugin-babel').default;
+const postcss = require('rollup-plugin-postcss');
 
 const currentWorkingPath = process.cwd();
 const { main, name } = require(path.join(currentWorkingPath, 'package.json'));
@@ -18,9 +19,13 @@ const inputOptions = {
   external: ['react'],
   plugins: [
     resolve(),
+    postcss({
+      modules: true,
+    }),
     babel({
-      presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+      presets: ['@babel/preset-env', '@babel/preset-react'],
       babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
     }),
   ],
 };
--- packages/phoenix-button/lib/phoenix-button.js
+++ packages/phoenix-button/lib/phoenix-button.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './styles.css';
 
-const Button = ({ children }) => <button>{children}</button>;
+const Button = ({ children }) => <button className={styles.Button}>{children}</button>;
 
 export { Button };
