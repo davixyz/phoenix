247bc631866d7093c509f6bad0da943848bc33f3
Carlos Castro <cdcastrofelix@gmail.com>
2020-06-10 21:37:58 -0700

    feat: add storybook and deployment mechanism

---
+++ packages/phoenix-button/docs/phoenix-button.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// We want to always get from source
+import { Button } from '../lib/phoenix-button';
+
+export default { title: 'Button' };
+
+export const primary = () => <Button>Hello Button</Button>;
---
+++ packages/phoenix-text/docs/phoenix-text.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// We want to always get from source
+import { Text } from '../lib/phoenix-text';
+
+export default { title: 'Text' };
+
+export const small = () => <Text>Hello Text</Text>;
